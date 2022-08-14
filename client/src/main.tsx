730cb2e101ab503e3eed6de5e9996fe30f4c7a51
Juris710 <juris710.dev@gmail.com>
2022-08-14 19:25:30 +0900

    Setup for production

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io("http://localhost:3000");
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = import.meta.env.DEV ? io("http://localhost:3000") : io();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import http from 'http';
+import path from 'path';
 import { Server } from "socket.io";
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+if (process.env["NODE_ENV"] === "production"){
+  app.use("/", 
+    express.static(
+      path.resolve(__dirname, "../dist_client")
+    )
+  );
+}
 const server = http.createServer(app);
 
 const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(server, {
@@ -20,3 +25,4 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+console.log(__dirname)

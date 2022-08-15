7e3e984627066a3828c726adb6486925d9cf9a0b
Juris710 <juris710.dev@gmail.com>
2022-08-15 20:18:36 +0900

    Improve styles

--- client/src/pages/Timer/index.tsx
+++ client/src/pages/Timer/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Center, Stack, Text } from "@mantine/core";
+import { Button, Center, Container, Group, Stack, Title } from "@mantine/core";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { startTimeMsState, timerTextState } from "../../recoil";
 import { TimerRunner } from "./Runner";
@@ -9,23 +9,32 @@ export const TimerPage: React.FC = () => {
   const timerText = useRecoilValue(timerTextState);
   const [startTimeMs, setStartTimeMs] = useRecoilState(startTimeMsState);
   return (
-    <Center>
-      <Stack>
-        <FinishNotifier />
-        <TimerRunner />
-        <Text>{timerText}</Text>
-        <Button
-          onClick={() => {
-            setStartTimeMs(Date.now());
-          }}
-          disabled={startTimeMs !== 0}
-        >
-          Start
-        </Button>
-        <Button onClick={() => setStartTimeMs(0)} disabled={startTimeMs === 0}>
-          Stop
-        </Button>
-      </Stack>
-    </Center>
+    <Container>
+      <FinishNotifier />
+      <TimerRunner />
+      <Center>
+        <Stack>
+          <Center>
+            <Title order={1}>{timerText}</Title>
+          </Center>
+          <Group>
+            <Button
+              onClick={() => {
+                setStartTimeMs(Date.now());
+              }}
+              disabled={startTimeMs !== 0}
+            >
+              Start
+            </Button>
+            <Button
+              onClick={() => setStartTimeMs(0)}
+              disabled={startTimeMs === 0}
+            >
+              Stop
+            </Button>
+          </Group>
+        </Stack>
+      </Center>
+    </Container>
   );
 };

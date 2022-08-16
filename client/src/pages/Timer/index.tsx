a91a4778682e90b2f7568e0eeb6464b74d3ca7ac
Juris710 <juris710.dev@gmail.com>
2022-08-16 16:34:55 +0900

    Delete comment

--- client/src/pages/Timer/index.tsx
+++ client/src/pages/Timer/index.tsx
@@ -8,21 +8,7 @@ import { AdminButtons } from "./AdminButtons";
 import { useParams } from "react-router-dom";
 import { socket } from "../../components/Sockets";
 import { milliseconds2timerText } from "../../utils";
-/*
-export const timerTextState = selector({
-  key: "timerTextState",
-  get: ({ get }) => {
-    const remainingMs = get(remainingMsState);
-    if (remainingMs === undefined) {
-      return milliseconds2timerText(0);
-    }
-    if (remainingMs > 0) {
-      return milliseconds2timerText(remainingMs);
-    }
-    return milliseconds2timerText(0);
-  },
-});
-*/
+
 export const TimerPage: React.FC<{ isAdmin?: boolean }> = ({
   isAdmin = false,
 }) => {

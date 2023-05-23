a65afea1070b7ea6bab14c70c7540460001a55a9
Juris710 <juris710.dev@gmail.com>
2023-05-23 23:28:08 +0900

    Fix CORS url

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ const io = new Server<
   SocketData
 >(server, {
   cors: {
-    origin: ["http://127.0.0.1:5173"],
+    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
   },
 });
 type TimerData = {

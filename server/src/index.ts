3f34f59252c885a1855adf2d60b35d8b4801f907
Juris710 <juris710.dev@gmail.com>
2023-05-24 23:32:36 +0900

    Set CORS urls only in development

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,9 +19,12 @@ const io = new Server<
   InterServerEvents,
   SocketData
 >(server, {
-  cors: {
-    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
-  },
+  cors:
+    process.env["NODE_ENV"] === "development"
+      ? {
+          origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+        }
+      : {},
 });
 type TimerData = {
   startTimeMs: number;
